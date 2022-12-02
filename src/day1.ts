96faf5610af71b80ad22a3c796276535baccff12
Ethan Dunn <Ethan05Dunn@gmail.com>
2022-12-01 22:21:01 -0600

    Add eslint & prettier b/c why not

--- src/day1.ts
+++ src/day1.ts
@@ -1,30 +1,30 @@
-import * as fs from 'fs'
-import { EOL } from 'os';
+import * as fs from "fs";
+import { EOL } from "os";
 
-const calorieDataRaw = fs.readFileSync('./inputs/day1.txt', 'utf8');
+const calorieDataRaw = fs.readFileSync("./inputs/day1.txt", "utf8");
 
-const calorieDataLines = calorieDataRaw.split(EOL)
+const calorieDataLines = calorieDataRaw.split(EOL);
 
-let elfCalories = [0];
+const elfCalories = [0];
 let currentElf = 0;
-for(let line of calorieDataLines) {
-    // Move to next elf when done with current elf
-    if(line === '') {
-        currentElf++;
-        elfCalories[currentElf] = 0;
-        continue;
-    }
+for (const line of calorieDataLines) {
+  // Move to next elf when done with current elf
+  if (line === "") {
+    currentElf++;
+    elfCalories[currentElf] = 0;
+    continue;
+  }
 
-    let cal = parseInt(line);
-    elfCalories[currentElf] += cal;
+  const cal = parseInt(line);
+  elfCalories[currentElf] += cal;
 }
 
-let topCal = Math.max(...elfCalories);
+const topCal = Math.max(...elfCalories);
 console.log(`Part 1: ${topCal}`);
 
 elfCalories.sort((a, b) => b - a);
-let topThree = elfCalories.slice(0, 3)
-let sumTopThree = topThree.reduce((p, c, i) => p + c);
+const topThree = elfCalories.slice(0, 3);
+const sumTopThree = topThree.reduce((p, c, i) => p + c);
 console.log(`Part 2: ${sumTopThree}`);
 
-debugger
+debugger;
